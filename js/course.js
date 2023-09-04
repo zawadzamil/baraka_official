a0e43c3bfb84ec98040cebbbc5def29e23686ba4
zawadzamil <contact.zawadzamil@gmail.com>
2023-09-04 21:51:11 +0600

    Feature Completed

--- js/course.js
+++ js/course.js
@@ -1,16 +1,30 @@
-$('#firstButton').click(function(){
+$('#firstButton').click(function () {
     $('#firstButton').css("background-color", "rgb(21, 21, 85)");
     $('#secondButton').css("background-color", "#0ca59d");
+    $('#thirdButton').css("background-color", "#0ca59d");
     $('#overview').show();
     $('#curriculum').hide();
+    $('.resources').hide();
 });
 
-$('#secondButton').click(function(){
+$('#secondButton').click(function () {
     $('#firstButton').css("background-color", "#0ca59d");
     $('#secondButton').css("background-color", "rgb(21, 21, 85)");
+    $('#thirdButton').css("background-color", "#0ca59d");
     $('#overview').hide();
     $('#curriculum').show();
+    $('.resources').hide();
 });
+
+$('#thirdButton').click(function () {
+    $('#firstButton').css("background-color", "#0ca59d");
+    $('#secondButton').css("background-color", "#0ca59d");
+    $('#thirdButton').css("background-color", "rgb(21, 21, 85)");
+    $('#overview').hide();
+    $('#curriculum').hide();
+    $('.resources').show();
+
+})
 
 
 //Getting Course Number From URL
@@ -19,22 +33,25 @@ const urlParams = new URLSearchParams(queryString);
 const course = urlParams.get('course')
 let courseNumber = parseInt(course);
 console.log(courseNumber);
+if (courseNumber != 1) {
+    $('#thirdButton').hide()
+}
 
 
 
 
 const courses = [
     {
-        "title": "Embedded System Design (Biggner)",
-        "id":1,
-        "image":"emdCourse1.png",
+        "title": "Embedded System Design (Beginner)",
+        "id": 1,
+        "image": "emdCourse1.png",
         "duration": 36,
         "lectures": 15,
         "skill_level": "Beginner",
-        "price":"15,000",
+        "price": "15,000",
         "about": "Embedded systems design is the process where hardware and firmware designers come together to build embedded systems from scratch. This involves PCB design, where the necessary components are connected to build functional circuits. To bring the electronics to life, the firmware is coded and then programmed into the microcontroller. ",
 
-        "description" : "This course is focused on giving you real world coding experience and hands on project work with ARM based Microcontrollers. You will learn how to implement software configuration management and develop embedded software applications.The course concludes with a project where you will create your own build system and firmware that can manipulate memory.",
+        "description": "This course is focused on giving you real world coding experience and hands on project work with ARM based Microcontrollers. You will learn how to implement software configuration management and develop embedded software applications.The course concludes with a project where you will create your own build system and firmware that can manipulate memory.",
 
         "certification": "After completing the course students will earn a certificate and will become proficient in C, Embedded C, Microcontrollers, Arduino, ESP32 etc ",
 
@@ -58,20 +75,20 @@ const courses = [
             ' Develop  exciting firmware'
 
         ],
-        "prerequisites":[
+        "prerequisites": [
             'Basic knowledge of PCs, MS Windows, internet and online proficiency',
             'Basic knowledge of English (reading/writing/speaking)',
             'Familiarity with coding (C/C++)',
             'Willingness to buy necessary components (like sensors, actuators etc)'
         ],
         "software":
-        [
-            'Arduino IDE',
-            'Platform IO',
-            'Thonny Python',
-            'VS Code'
-        ],
-        "module":[
+            [
+                'Arduino IDE',
+                'Platform IO',
+                'Thonny Python',
+                'VS Code'
+            ],
+        "module": [
             "Introduction C-Programs with ARM Core Microcontrollers",
             "Introduction to Microcontrollers",
             "Basic Functionalities & Interfacing of Arduino",
@@ -89,12 +106,12 @@ const courses = [
     },
     {
         "title": "Advanced Embedded System Design (Advanced)",
-        "id":2,
-        "image":"adEmdCource2.png",
+        "id": 2,
+        "image": "adEmdCource2.png",
         "duration": 40,
         "lectures": 25,
         "skill_level": "Advanced",
-        "price":"25,000",
+        "price": "25,000",
         "about": "The Real-Time Embedded Systems specialization. It is an advanced course that will be taking you from a beginning practitioner, to a more advanced real-time system analyst and designer.  Knowledge and experience gained on hard to master topics such as predictable response services, when to allocate requirements to hardware or software, as well as mission critical design will enhance your engineering talent.",
 
         "description": "By doing  this course, you can consider yourself an intermediate to more advanced real-time system practitioner.  This knowledge is invaluable for medical, aerospace, transportation, energy, digital entertainment, telecommunications, and other exciting embedded career options.",
@@ -103,13 +120,13 @@ const courses = [
 
         "outcomes": [
             'Learn The Real-Time Embedded Systems specialization ',
-           'Advanced real-time system analyst and designer',
-           'Predictable response services',
-           'Experience building a simple, but real, system project with real-time challenges',
-           'You will consider yourself an intermediate to more advanced real-time system practitioner',
-           'Hands-on practice and assessment of your learning progress',
-           'Put theory into practice and how to evaluate design options and make optimal choices',
-           'See real-time challenges with your eyes, to debug interactively, and build a simple at-home detection, tracking and synchronization system.'
+            'Advanced real-time system analyst and designer',
+            'Predictable response services',
+            'Experience building a simple, but real, system project with real-time challenges',
+            'You will consider yourself an intermediate to more advanced real-time system practitioner',
+            'Hands-on practice and assessment of your learning progress',
+            'Put theory into practice and how to evaluate design options and make optimal choices',
+            'See real-time challenges with your eyes, to debug interactively, and build a simple at-home detection, tracking and synchronization system.'
 
 
         ],
@@ -123,7 +140,7 @@ const courses = [
             'Python'
 
         ],
-        "prerequisites":[
+        "prerequisites": [
             'Good knowledge of Microcontrollers',
             'Basic knowledge of English (reading/writing/speaking)',
             'Good Understanding in  coding (C/C++)',
@@ -132,14 +149,14 @@ const courses = [
 
         ],
         "software":
-        [
-            'Arduino IDE',
-            'Platform IO',
-            'Thonny Python',
-            'VS Code',
-          
-        ],
-        "module":[
+            [
+                'Arduino IDE',
+                'Platform IO',
+                'Thonny Python',
+                'VS Code',
+
+            ],
+        "module": [
             "Exploration of RT Challenges and Concepts",
             "Real-time systems and scheduling",
             "Utility curves used for analysis of real-time systems ",
@@ -147,19 +164,19 @@ const courses = [
             "IoT Project using Raspberry PI",
             "Full Home Automation",
             "Overview of Real-time Hardware Architectures and Software Stacks"
-            
+
 
         ]
-   
+
     },
     {
-        "title": "Responsive Web Design (Biggner)",
-        "id":3,
-        "image":"webCoursce3.png",
+        "title": "Responsive Web Design (Beginner)",
+        "id": 3,
+        "image": "webCoursce3.png",
         "duration": 40,
         "lectures": 25,
         "skill_level": "Beginner",
-        "price":"15,000",
+        "price": "15,000",
         "about": "In this course, you will learn how to apply concepts from interaction design and human-computer interaction in order to design and build an interactive, professional-looking website. You will learn how to make your web page designs adapt to different screen sizes using responsive grid layouts. You will learn how to add navigation and other design elements, and you will learn how to separate data and display using JavaScript objects and templates. You’ll also learn about jQuery plugins and basic Word Press Theme Customization.",
 
         "description": "In this course, you will learn how to apply concepts from interaction design and human-computer interaction in order to design and build an interactive, professional-looking website. You will learn how to make your web page designs adapt to different screen sizes using responsive grid layouts. You will learn how to add navigation and other design elements, and you will learn how to separate data and display using JavaScript objects and templates. You’ll also learn about jQuery plugins and basic Word Press Theme Customization.",
@@ -168,20 +185,20 @@ const courses = [
 
         "outcomes": [
             'Create HTML layout',
-           'Create a sign in and sign up a page with a navigation bar',
-           'Make Multipage website',
-           'Make PSD to Responsive Design',
-           'Understand and be able to apply media queries and breakpoints',
-           'Add responsive jQuery plugins such as sliding carousels and social share buttons',
-           'Create Landing Page',
-           'Create E-commerce website',
-           'Full Theme customization',
-           'Build a responsive website from scratch or by using Bootstrap',
-           'Create HTML layout',
-           'Make Multipage website',
-           'Create an E-commerce website with a premium Wp theme',
-           'Javascript Programming',
-           'Responsive Design with CSS'
+            'Create a sign in and sign up a page with a navigation bar',
+            'Make Multipage website',
+            'Make PSD to Responsive Design',
+            'Understand and be able to apply media queries and breakpoints',
+            'Add responsive jQuery plugins such as sliding carousels and social share buttons',
+            'Create Landing Page',
+            'Create E-commerce website',
+            'Full Theme customization',
+            'Build a responsive website from scratch or by using Bootstrap',
+            'Create HTML layout',
+            'Make Multipage website',
+            'Create an E-commerce website with a premium Wp theme',
+            'Javascript Programming',
+            'Responsive Design with CSS'
 
 
         ],
@@ -197,113 +214,113 @@ const courses = [
             'How to communicate with the client',
             'How to bid and win jobs',
             'How to manage projects and receive high ratings',
-            
+
 
         ],
-        "prerequisites":[
+        "prerequisites": [
             'Basic knowledge of PCs, MS Windows, internet and online proficiency',
             'Basic knowledge of English (reading/writing/speaking)',
             'Familiarity with coding (for HTML/ CSS etc.)',
-            
+
 
         ],
         "software":
-        [
-            'Notepad',
-            'Sublime Text',
-            'VS Code',
-            'Photoshop',
-          
-        ],
-        "module":[
+            [
+                'Notepad',
+                'Sublime Text',
+                'VS Code',
+                'Photoshop',
+
+            ],
+        "module": [
             "Introduction of Website",
-          'Introduction with HTML',
-          'Creating web layout using HTML & CSS',
-          'Single page website concept',
-          'Sign in & Sign up page with navbar',
-          'Responsive Page using media query',
-          'CSS3 effects & implementation',
-          'Multipage website creation',
-          'Introduction with Bootstrap & responsive web design',
-          'Responsive single page website creation',
-          'One page responsive design with navbar & carousel',
-          'Adding Bootstrap JavaScript components',
-          'PSD to Responsive Website Creation (Using Html, CSS & Bootstrap)',
-          'Introduction with JavaScript (Syntax, Variable, Expression & Condition)',
-          'Implementing JavaScript on Website',
-          'jQuery Effects & Uses',
-          'jQuery Plugins Integration in Website',
-          'Introduction with WordPress & Installation',
-          'Creating Landing Page using Paid Theme',
-          'Full Theme Customization',
-          'Form Creation & Integration Using Mail Chimp',
-          'Post Sharing Method',
-          'How To Create A eCommerce Website',
-          'eCommerce Website With Premium Theme',
-          
-            
+            'Introduction with HTML',
+            'Creating web layout using HTML & CSS',
+            'Single page website concept',
+            'Sign in & Sign up page with navbar',
+            'Responsive Page using media query',
+            'CSS3 effects & implementation',
+            'Multipage website creation',
+            'Introduction with Bootstrap & responsive web design',
+            'Responsive single page website creation',
+            'One page responsive design with navbar & carousel',
+            'Adding Bootstrap JavaScript components',
+            'PSD to Responsive Website Creation (Using Html, CSS & Bootstrap)',
+            'Introduction with JavaScript (Syntax, Variable, Expression & Condition)',
+            'Implementing JavaScript on Website',
+            'jQuery Effects & Uses',
+            'jQuery Plugins Integration in Website',
+            'Introduction with WordPress & Installation',
+            'Creating Landing Page using Paid Theme',
+            'Full Theme Customization',
+            'Form Creation & Integration Using Mail Chimp',
+            'Post Sharing Method',
+            'How To Create A eCommerce Website',
+            'eCommerce Website With Premium Theme',
+
+
 
         ]
-   
+
     },
     {
         "title": "Advanced Web Development (Comming Soon)",
-        "id":4,
-        "image":"awdCourse4.png",
+        "id": 4,
+        "image": "awdCourse4.png",
         "duration": 40,
         "lectures": 25,
         "skill_level": "Advanced",
-        "price":"(TBD)",
+        "price": "(TBD)",
         "about": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus provident velit quasi porro consequatur eaque ipsa fuga sed reprehenderit beatae quidem quisquam, aliquid molestias et doloremque ipsam vitae sit, sint laboriosam maiores. Iure ut expedita tenetur suscipit eveniet? Unde esse iusto eaque, amet nesciunt vel cupiditate soluta quidem. Numquam, sit"
     }
 ]
 
-$('#firstImage').attr("src", "images/"+courses[courseNumber-1].image);
-$('#secondImage').attr("src", "images/"+courses[courseNumber-1].image);
-$('#title').text(courses[courseNumber-1].title);
-$('#subtitle').text(courses[courseNumber-1].title);
-$('#mainTitle').text(courses[courseNumber-1].title);
-$('#about').text(courses[courseNumber-1].about);
-$('#duration').text(courses[courseNumber-1].duration +' Hours');
-$('#lectures').text(courses[courseNumber-1].lectures);
-$('#skill_level').text(courses[courseNumber-1].skill_level);
-$('#price').text(courses[courseNumber-1].price);
-$('#price2nd').text(courses[courseNumber-1].price);
-$('#course_description').text(courses[courseNumber-1].description);
-$('#certification').text(courses[courseNumber-1].certification);
+$('#firstImage').attr("src", "images/" + courses[courseNumber - 1].image);
+$('#secondImage').attr("src", "images/" + courses[courseNumber - 1].image);
+$('#title').text(courses[courseNumber - 1].title);
+$('#subtitle').text(courses[courseNumber - 1].title);
+$('#mainTitle').text(courses[courseNumber - 1].title);
+$('#about').text(courses[courseNumber - 1].about);
+$('#duration').text(courses[courseNumber - 1].duration + ' Hours');
+$('#lectures').text(courses[courseNumber - 1].lectures);
+$('#skill_level').text(courses[courseNumber - 1].skill_level);
+$('#price').text(courses[courseNumber - 1].price);
+$('#price2nd').text(courses[courseNumber - 1].price);
+$('#course_description').text(courses[courseNumber - 1].description);
+$('#certification').text(courses[courseNumber - 1].certification);
 
-const outcomes  = courses[courseNumber-1].outcomes;
+const outcomes = courses[courseNumber - 1].outcomes;
 $("#learning_outcome").empty();
 outcomes.forEach(element => {
-    $("#learning_outcome").append('<li>'+ element+'</li>');
-    
+    $("#learning_outcome").append('<li>' + element + '</li>');
+
 });
-const highlight  = courses[courseNumber-1].highlight;
+const highlight = courses[courseNumber - 1].highlight;
 $("#highlight").empty();
 highlight.forEach(element => {
-    $("#highlight").append('<li>'+ element+'</li>');
-    
+    $("#highlight").append('<li>' + element + '</li>');
+
 });
 
 
-const prerequisites  = courses[courseNumber-1].prerequisites;
+const prerequisites = courses[courseNumber - 1].prerequisites;
 $("#prerequisites").empty();
 prerequisites.forEach(element => {
-    $("#prerequisites").append('<li>'+ element+'</li>');
-    
+    $("#prerequisites").append('<li>' + element + '</li>');
+
 });
 
-const software  = courses[courseNumber-1].software;
+const software = courses[courseNumber - 1].software;
 $("#software").empty();
 software.forEach(element => {
-    $("#software").append('<li>'+ element+'</li>');
-    
+    $("#software").append('<li>' + element + '</li>');
+
 });
 
-const module  = courses[courseNumber-1].module;
+const module = courses[courseNumber - 1].module;
 $("#module").empty();
 module.forEach(element => {
-    $("#module").append('<li>'+ element+'</li>');
-    
+    $("#module").append('<li>' + element + '</li>');
+
 });
 
